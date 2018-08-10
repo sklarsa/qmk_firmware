81225090785b7cd0d28e84ef7e3b956f7a67e1d0
Steven Sklar <steve@predata.com>
2018-08-10 17:39:25 -0400

    More standard layout

--- keyboards/ergodox_ez/keymaps/steve/keymap.c
+++ keyboards/ergodox_ez/keymaps/steve/keymap.c
@@ -26,32 +26,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
       // left hand
       KC_ESC,            KC_1,           KC_2,       KC_3,        KC_4,       KC_5,      KC_CAPSLOCK,
-      KC_TAB,            KC_Q,           KC_W,       KC_E,        KC_R,       KC_T,      KC_LBRC,
-      LT(ARRSUBL,KC_EQL),KC_A,           KC_S,       KC_D,        KC_F,       KC_G,
-      OSM(MOD_LSFT),     KC_Z,           KC_X,       KC_C,        KC_V,       KC_B,      KC_MINS,
-      OSM(MOD_LCTL),     ALT_T(KC_QUOT), ALT_CMD,    KC_LEFT,     KC_RIGHT,
+      KC_TAB,            KC_Q,           KC_W,       KC_E,        KC_R,       KC_T,      KC_TRNS,
+      LT(2,KC_ESC),      KC_A,           KC_S,       KC_D,        KC_F,       KC_G,
+      OSM(MOD_LSFT),     KC_Z,           KC_X,       KC_C,        KC_V,       KC_B,      KC_TRNS,
+      OSM(MOD_LCTL),     ALT_T(KC_BSLS), ALT_CMD,    KC_LEFT,     KC_RIGHT,
                                                                   KC_TRNS,    TT(MEDIA),
                                                                               KC_TRNS,
-                                               LT(CHARNUM,KC_TAB),KC_BSPC,    KC_DELETE,
+                                                OSL(CHARNUM), OSM(MOD_LGUI),  KC_DELETE,
 
 
       // right hand
       KC_ESC,     KC_6,       KC_7,       KC_8,       KC_9,     KC_0,            KC_BSPC,
-      KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,     KC_P,            KC_BSLS,
-                  KC_H,       KC_J,       KC_K,       KC_L,     LT(2,KC_SCOLON), GUI_T(KC_QUOT),
-      KC_UNDS,    KC_N,       KC_M,       KC_COMMA,   KC_DOT,   RCTL_T(KC_SLASH),KC_RSHIFT,
+      KC_TRNS,    KC_Y,       KC_U,       KC_I,       KC_O,     KC_P,            KC_QUOT,
+                  KC_H,       KC_J,       KC_K,       KC_L,     LT(2,KC_SCOLON), RGUI_T(KC_ENTER),
+      KC_TRNS,    KC_N,       KC_M,       KC_COMMA,   KC_DOT,   RCTL_T(KC_SLASH),OSM(MOD_RSFT),
                               KC_UP,      KC_DOWN,    KC_LBRC,  KC_RBRC,         TG(2),
       KC_TRNS,    KC_TRNS,
       KC_TRNS,
-      KC_TRNS,    KC_ENTER,   GUI_T(KC_SPACE)),
+      KC_TRNS,    GUI_T(KC_ENTER),   KC_SPACE),
 
   // Numpad, function keys, and shifted punctuation
   [CHARNUM] = LAYOUT_ergodox(
     // left hand
     KC_ESC,   KC_F1,   KC_F2,   KC_F3,     KC_F4,    KC_F5,    KC_TRNS,
-    KC_TRNS,  KC_EXLM, KC_AT,   KC_LCBR,   KC_RCBR,  KC_PIPE,  KC_TRNS,
-    KC_TRNS,  KC_HASH, KC_DLR,  KC_LPRN,   KC_RPRN,  KC_GRV,
-    KC_TRNS,  KC_PERC, KC_CIRC, KC_LBRC,   KC_RBRC,  KC_TILD,  KC_TRNS,
+    KC_TRNS,  KC_ASTR, KC_SLASH,KC_LCBR,   KC_RCBR,  KC_PIPE,  KC_TRNS,
+    KC_TRNS,  KC_EQL,  KC_PLUS, KC_LPRN,   KC_RPRN,  KC_GRV,
+    KC_TRNS,  KC_MINS, KC_UNDS, KC_LBRC,   KC_RBRC,  KC_TILD,  KC_TRNS,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
                                                      KC_TRNS,  KC_TRNS,
                                                                KC_TRNS,
@@ -59,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // right hand
     KC_TRNS,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,    KC_F11,
-    KC_TRNS,  KC_TRNS,KC_7,   KC_8,   KC_9,   KC_ASTR,   KC_F12,
-              KC_TRNS,KC_4,   KC_5,   KC_6,   KC_PLUS,   KC_TRNS,
-    KC_TRNS,  KC_AMPR,KC_1,   KC_2,   KC_3,   KC_BSLS,   KC_TRNS,
-                      KC_EQL, KC_0,   KC_DOT, KC_MINS,   KC_TRNS,
+    KC_TRNS,  KC_AMPR,KC_7,   KC_8,   KC_9,   KC_ASTR,   KC_F12,
+              KC_QUOT,KC_4,   KC_5,   KC_6,   KC_PLUS,   KC_TRNS,
+    KC_TRNS,  KC_DQT, KC_1,   KC_2,   KC_3,   KC_BSLS,   KC_TRNS,
+                      KC_EQL, KC_0,   KC_TRNS,KC_MINS,  KC_TRNS,
     KC_TRNS,   KC_TRNS,
     KC_TRNS,
     KC_TRNS,KC_TRNS, KC_TRNS),
